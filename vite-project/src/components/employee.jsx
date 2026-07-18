02d81dbd39cfaf06a0ebbe862f29dc37383804af
Gayathri <gayathrithota66@gmail.com>
2026-07-18 16:03:02 +0530

    added file

---
+++ vite-project/src/components/employee.jsx
@@ -0,0 +1,14 @@
+function Employee() {
+  return (
+    <div>
+      <h2>Employee Details</h2>
+
+      <p>Employee ID: 101</p>
+      <p>Employee Name: Gayathri</p>
+      <p>Department: Java</p>
+      <p>Salary: ₹35000</p>
+    </div>
+  );
+}
+
+export default Employee;
